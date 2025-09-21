c9c7fdaeca4d953f2d7c97af0eb6359f7c0a9abe
Rushit Patel <rushitpatel163@gmail.com>
2025-09-22 00:22:00 +0530

    Footer updated

--- app/packages/[slug]/page.tsx
+++ app/packages/[slug]/page.tsx
@@ -615,7 +615,7 @@ export default function PackagePage({
                 the world.
               </p>
               <div className="flex gap-4">
-                <Link href="#" className="text-white/80 hover:text-white">
+                {/* <Link href="#" className="text-white/80 hover:text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -630,8 +630,8 @@ export default function PackagePage({
                   >
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                   </svg>
-                </Link>
-                <Link href="#" className="text-white/80 hover:text-white">
+                </Link> */}
+                <Link href="https://www.instagram.com/circletrip_holidays/" className="text-white/80 hover:text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -656,7 +656,7 @@ export default function PackagePage({
                     <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                   </svg>
                 </Link>
-                <Link href="#" className="text-white/80 hover:text-white">
+                {/* <Link href="#" className="text-white/80 hover:text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -671,7 +671,7 @@ export default function PackagePage({
                   >
                     <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                   </svg>
-                </Link>
+                </Link> */}
               </div>
             </div>
 
@@ -767,7 +767,7 @@ export default function PackagePage({
                     <circle cx="12" cy="10" r="3"></circle>
                   </svg>
                   <span className="text-white/80">
-                    123 Travel Street, City, Country
+                    1572,sector 24, Panipat, Haryana, India, 132103
                   </span>
                 </li>
                 <li className="flex items-start gap-3">
@@ -785,7 +785,7 @@ export default function PackagePage({
                   >
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                   </svg>
-                  <span className="text-white/80">+1 (234) 567-8900</span>
+                  <span className="text-white/80">+91 9518812344</span>
                 </li>
                 <li className="flex items-start gap-3">
                   <svg
@@ -803,6 +803,7 @@ export default function PackagePage({
                     <rect x="2" y="4" width="20" height="16" rx="2"></rect>
                     <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                   </svg>
+                <span className="text-white/80">holidays.circletrip@gmail.com</span>
                 </li>
               </ul>
             </div>
